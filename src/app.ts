920a479d399eecc338c8306b4a4175568f626c10
Jonas Gomes de Lima Neto <jotajonas09@gmail.com>
2026-07-23 12:10:10 -0300

    Corrigir rota GET StartPage (pageRoutes)

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
 app.use(pageRoutes)
 
-app.use(express.static("public"));
 export default app;
