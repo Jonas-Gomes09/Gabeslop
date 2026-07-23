82c53e601240547b6e627b467cc5f57aefcf0661
João Marvin Vieira Garcia <joaomarvim99@gmail.com>
2026-07-23 12:46:49 -0300

    Adicionar Middlewares

--- src/models/userRepository.ts
+++ src/models/userRepository.ts
@@ -1,4 +1,4 @@
-import {Usuario} from "../entities/comentario"
+import {user} from "../entities/user"
 import { readFile, writeFile, mkdir } from "fs/promises"
 
 export class userRepository {
@@ -10,11 +10,11 @@ export class userRepository {
         this.directory = directory
     }
     // Carregar usuários (SERVIDOR)
-    private async loadUsers(): Promise<Usuario[]> {
+    private async loadUsers(): Promise<user[]> {
         try {
             const content = await readFile(this.usersFile, "utf-8");
             const parsedContent = JSON.parse(content)
-            return parsedContent.map((d: any) => {Usuario.fromJSON(d)})
+            return parsedContent.map((d: any) => {user.fromJSON(d)})
         } catch {
             console.log("userRepository loadFiles() | Não há nenhum usuário cadastrado no banco de dados.")
             await this.saveUsers([]);
@@ -23,7 +23,7 @@ export class userRepository {
     }
 
     // Salvar os usuários (SERVIDOR)
-    private async saveUsers(users: Usuario[]): Promise<void> {
+    private async saveUsers(users: user[]): Promise<void> {
         try {
             const json = users.map(u=> u.toJSON());
             await writeFile(this.usersFile, JSON.stringify(json, null, 2))
@@ -33,14 +33,14 @@ export class userRepository {
     }
 
     // Adicionar usuário (LOGIN DO CLIENTE)
-    async addUser(titulo: string, usuario: string, nota: number, dataCriacao: string, totalCompras: number, foto: string | null = null): Promise<Usuario> {
-        const erros = Usuario.validar({ titulo, usuario, nota})
+    async addUser(nome: string, email: string, senha: string, dataCriacao: string, totalCompras: number, foto: string | null = null): Promise<user> {
+        const erros = user.validar({nome, email, senha})
 
         if(erros.length > 0) throw new Error(erros.join(", "))
         dataCriacao = `${new Date().toLocaleTimeString()} | ${new Date().toLocaleDateString()}`
         const users = await this.loadUsers()
         const nextID = (users.length > 0 ? users[users.length - 1].id : 0) + 1;
-        const newUser = new Usuario(nextID, titulo, usuario, nota, dataCriacao, totalCompras = 0, foto)
+        const newUser = new user(nextID, nome, email, senha, dataCriacao, totalCompras = 0, foto)
         users.push(newUser)
         await this.saveUsers(users)
         return newUser
@@ -59,17 +59,17 @@ export class userRepository {
     }
 
     // Listar todos (PAINEL DE MODERADOR)
-    async listAll(searchTerm?: string): Promise<Usuario[]> {
+    async listAll(searchTerm?: string): Promise<user[]> {
         let users = await this.loadUsers()
         if (searchTerm && searchTerm.trim()) {
             const lowercase = searchTerm.toLowerCase()
-            users = users.filter(u => u.titulo.toLowerCase().includes(lowercase))
+            users = users.filter(u => u.nome.toLowerCase().includes(lowercase))
         }
         return users
     }
 
     // Procurar por ID (PAINEL DE MODERADOR / INFORMAÇÕES DA CONTA DO CLIENTE)
-    async userInfo(id: number): Promise<Usuario | undefined> {
+    async userInfo(id: number): Promise<user | undefined> {
         const users = await this.loadUsers()
         const filter = users.find(u => u.id === id)
         
