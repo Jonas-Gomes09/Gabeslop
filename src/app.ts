ee996f3af936d486dc43e19726d761d78c0f3de9
João Marvin Vieira Garcia <joaomarvim99@gmail.com>
2026-07-24 11:34:27 -0300

    Adicionar pageRoute GET e alteração no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,32 @@
 import express from "express";
 import session from "express-session";
+import path from "path";
 import { pageRoutes } from "./routes/pageRoutes";
 import { logger } from "./middlewares/logger";
 
 const app = express();
 
-app.use(session({
+app.use(
+  session({
     secret: "segredo-senac-2026-rafavicnajonasmarvin",
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 30 * 60 * 1000,
-        httpOnly: true,
-        secure: false
-    }
+      maxAge: 30 * 60 * 1000,
+      httpOnly: true,
+      secure: false,
+    },
   })
 );
 
+app.use(logger);
 
-app.use(logger)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
-app.use(pageRoutes)
+
+app.use(pageRoutes);
 
 export default app;
-
