7035d2270752024cdf2e54052742d2d2492067dd
Rafael Dias <rafaeldiasdesa09@gmail.com>
2026-07-28 11:14:56 -0300

    criação da tela de registro

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,8 @@ const PORT = 3009;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+const http = require('http')
+
+const express = require('express')
+
+http.createServer(express).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
