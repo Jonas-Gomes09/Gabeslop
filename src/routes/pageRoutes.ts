3d2600bb8d0e0d0499f23f32480e4df6d6f33398
João Marvin Vieira Garcia <joaomarvim99@gmail.com>
2026-07-31 11:36:39 -0300

    Adicionar administrador ejs

--- src/routes/pageRoutes.ts
+++ src/routes/pageRoutes.ts
@@ -1,33 +1,28 @@
 import { Router, Request, Response } from "express";
-import * as clientController from "../controllers/clientController"
+import * as clientController from "../controllers/clientController";
 import { admin, auth } from "../middlewares/auth";
 
 export const pageRoutes = Router();
 
-
 // ROTA PARA TODOS
 pageRoutes.get("/", clientController.StartPage);
-
 pageRoutes.get("/login", clientController.LoginPage);
-
 pageRoutes.get("/registro", clientController.RegisterPage);
-
 
 // ROTAS CLIENTE
 pageRoutes.get("/perfil", auth, (req: Request, res: Response) => {
-    res.render("perfil");
+  res.render("perfil");
 });
 
 pageRoutes.get("/store", auth, (req: Request, res: Response) => {
-    res.render("store");
+  res.render("store");
 });
 
 pageRoutes.get("/carrinho", auth, (req: Request, res: Response) => {
-    res.render("carrinho");
+  res.render("carrinho");
 });
 
-
 // ROTAS ADMIN
-pageRoutes.get("/admin/startpage", admin, (req: Request, res: Response) => {
-
-})
+pageRoutes.get("/admin", admin, (req: Request, res: Response) => {
+  res.render("administrador");
+});
